d81502b7f3eb56aaf8b8e3e87cf18d3984987e2e
MuhammedMiiRanna <merali.med@gmail.com>
2023-07-23 21:17:24 +0100

    Update main.tsx
    Add few comments

--- react-app-tuto/src/main.tsx
+++ react-app-tuto/src/main.tsx
@@ -3,7 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Here we are using ReactDOM to render the component tree down below
+// inside the element of the id of the root 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+  // This one of the built-in components in React, doesn't have a visual representation
+  // Its purpose is to identify potential problem
+
+  // we're taking this tree in rendering or displaying it 
+  // inside an  element with the id of root using ReactDOM library
+
+  // we can also render this component tree in a mobile app using 
+  // another library called React Native 
   <React.StrictMode>
     <App />
   </React.StrictMode>,
